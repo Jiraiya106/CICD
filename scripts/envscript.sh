5e81e1d83b73c2922ad67b594a7b6739e955f8c8
Elisey <Jiraiya106@gmail.com>
2021-09-22 19:37:13 +0300

    CodeDeploy AppSpec 3.1

--- scripts/envscript.sh
+++ scripts/envscript.sh
@@ -4,4 +4,4 @@ REGION="eu-west-1"
 WEB_DIR="/var/www/html"
 
 
-sudo bash -c "aws ssm get-parameters --name $PARAMATER --region $REGION --with-decryption --output text --query Parameters[].Value > $WEB_DIR/.env"
+sudo bash -c "aws ssm get-parameters --name $PARAMATER --region $REGION --with-decryption --output text --query Parameters[].Value > $WEB_DIR/env"
